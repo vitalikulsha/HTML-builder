a622e12ccdd44c5b81c1b1a657c47688816d6cbb
vitalikulsha <kulsha_85@mail.ru>
2022-11-03 20:50:38 +0300

    feat: implement index.js to 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const path = require('path');
+const fs = require('fs');
+
+fs.mkdir(
+  path.join(__dirname, 'files-copy'),
+  { recursive: true },
+  (err) => {
+    if (err) throw err;
+  }
+);
+
+fs.readdir(
+  path.join(__dirname, 'files'),
+  (err, files) => {
+    if (err) throw err;
+    for (let file of files) {
+      fs.copyFile(
+        path.join(__dirname, 'files', file),
+        path.join(__dirname, 'files-copy', file),
+        err => {
+          if (err) throw err;
+        })
+    };
+  });
+
+
 
