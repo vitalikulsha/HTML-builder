1ac9b813f0d0afcd75f8c95a8067b8db789aefee
vitalikulsha <kulsha_85@mail.ru>
2022-11-03 21:05:32 +0300

    refactor: update index.js

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,14 +2,16 @@ const path = require('path');
 const fs = require('fs');
 const folderPath = path.join(__dirname, 'secret-folder');
 
-const fileList = fs.readdirSync(folderPath, err => {
+fs.readdir(folderPath, (err, files) => {
   if (err) throw err;
-}).map(file => path.join(folderPath, file))
-  .filter(file => fs.lstatSync(file).isFile());
-
-for (let file of fileList) {
-  const fileExt = path.extname(file);
-  const fileName = path.basename(file).slice(0, -fileExt.length);
-  const fileSize = (fs.lstatSync(file).size / 1024).toFixed(2);
-  console.log(`${fileName} - ${fileExt.slice(1)} - ${fileSize}kb`);
-}
+  files = files.map(file => path.join(folderPath, file))
+    .filter(file => fs.lstatSync(file).isFile());
+  for (let file of files) {
+    const fileExt = path.extname(file);
+    const fileName = path.basename(file).slice(0, -fileExt.length);
+    fs.lstat(file, (err, stats) => {
+      if (err) throw err;
+      console.log(`${fileName} - ${fileExt.slice(1)} - ${(stats.size / 1024).toFixed(3)}kb`);
+    });
+  }
+});
